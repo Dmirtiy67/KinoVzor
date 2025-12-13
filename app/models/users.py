42b6f66cf038efaf32c2429d620b630faf97743f
oshdbaru <piptdastr@gmail.com>
2025-12-13 13:52:21 +0300

    delete

--- app/models/users.py
+++ app/models/users.py
@@ -13,7 +13,7 @@ class UserModel(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
-    hashed_password: Mapped[str] = mapped_column(String(300), nullable=False)
-
+    hashed_password: Mapped[str] = mapped_column(String(200), nullable=False)
     role_id: Mapped[int] = mapped_column(ForeignKey("roles.id"), nullable=False)
+    
     role: Mapped["RoleModel"] = relationship(back_populates="users")
